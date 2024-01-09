e23fb253224bc8b8d2fc970f4f40197efaebec3d
Ava Pun <a5pun@uwaterloo.ca>
2024-01-09 08:52:06 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+class App:
+    def __init__(self):
+        pass
+
+    def run(self) -> None:
+        pass
+
+
+if __name__ == '__main__':
+    app = App()
+    app.run()
---
+++ test.py
@@ -0,0 +1,6 @@
+from main import App
+
+
+def test_app():
+    app = App()
+    app.run()
