02a306bd6e0ac9eb6c40056e5d4c331921d6d081
Ava Pun <37046477+AvaLovelace1@users.noreply.github.com>
2025-06-01 15:03:00 -0700

    Add Tauri

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,31 @@ import {svelteTesting} from '@testing-library/svelte/vite';
 import {sveltekit} from '@sveltejs/kit/vite';
 import {defineConfig} from 'vite';
 
+const host = process.env.TAURI_DEV_HOST;
+
 export default defineConfig({
     plugins: [tailwindcss(), sveltekit()],
+    // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+    //
+    // 1. prevent vite from obscuring rust errors
+    clearScreen: false,
+    // 2. tauri expects a fixed port, fail if that port is not available
+    server: {
+        port: 1420,
+        strictPort: true,
+        host: host || false,
+        hmr: host
+            ? {
+                protocol: "ws",
+                host,
+                port: 1421,
+            }
+            : undefined,
+        watch: {
+            // 3. tell vite to ignore watching `src-tauri`
+            ignored: ["**/src-tauri/**"],
+        },
+    },
     test: {
         workspace: [{
             extends: './vite.config.ts',
