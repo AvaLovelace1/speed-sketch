ee49304f2a30b0a4c7c0f0c25205b886ef86e9bf
Ava Pun <37046477+AvaLovelace1@users.noreply.github.com>
2025-06-14 14:23:44 -0700

    Add window state restoration

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -51,6 +51,7 @@ fn is_img_file(entry: &DirEntry) -> bool {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_window_state::Builder::new().build())
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_opener::init())
