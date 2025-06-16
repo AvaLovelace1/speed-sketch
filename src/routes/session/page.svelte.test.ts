38e6e3ebb8149fe826026893e4de9a8b1555fe45
Ava Pun <37046477+AvaLovelace1@users.noreply.github.com>
2025-06-16 00:52:03 -0700

    delete some tests

--- src/routes/session/page.svelte.test.ts
+++ src/routes/session/page.svelte.test.ts
@@ -1,20 +1,9 @@
-import { describe, expect, test, vi } from 'vitest';
+import { describe, expect, test } from 'vitest';
 import '@testing-library/jest-dom/vitest';
 import { render, screen } from '@testing-library/svelte';
-import userEvent from '@testing-library/user-event';
 import { createRawSnippet } from 'svelte';
-import SessionUI from './SessionUI.svelte';
-import Toolbar from './Toolbar.svelte';
 import StatusAlert from './StatusAlert.svelte';
 import Timer from './Timer.svelte';
-
-describe.concurrent('SessionUI.svelte', () => {
-    test('session UI renders', async () => {
-        render(SessionUI);
-        expect(screen.getByRole('img')).toBeVisible();
-        expect(screen.getByRole('timer', { name: /.*remaining.*/i })).toBeVisible();
-    });
-});
 
 describe('StatusAlert.svelte', () => {
     test('alert renders', () => {
@@ -58,45 +47,5 @@ describe('Timer.svelte', () => {
     test('custom class is applied', async () => {
         render(Timer, { time: 10, class: 'custom-class' });
         expect(screen.getByRole('timer')).toHaveClass('custom-class');
-    });
-});
-
-describe('Toolbar.svelte', () => {
-    test('menu buttons work', async () => {
-        const handler1 = vi.fn();
-        const handler2 = vi.fn();
-        render(Toolbar, {
-            tools: [
-                { key: 1, label: 'Tool 1', icon: 'pause', action: handler1 },
-                { key: 2, label: 'Tool 2', action: handler2 },
-            ],
-        });
-
-        const tool1 = screen.getByRole('button', { name: 'Tool 1' });
-        const tool2 = screen.getByRole('button', { name: 'Tool 2' });
-        const user = userEvent.setup();
-        await user.click(tool1);
-        await user.click(tool2);
-        await user.click(tool2);
-        expect(handler1).toHaveBeenCalledTimes(1);
-        expect(handler2).toHaveBeenCalledTimes(2);
-    });
-    test('custom class is applied', () => {
-        const handler1 = vi.fn();
-        render(Toolbar, {
-            tools: [{ key: 1, label: 'Tool 1', action: handler1, class: 'custom-class' }],
-        });
-
-        expect(screen.getByRole('button', { name: 'Tool 1' })).toHaveClass('custom-class');
-    });
-    test('hotkeys work', async () => {
-        const handler1 = vi.fn();
-        render(Toolbar, {
-            tools: [{ key: 1, label: 'Tool 1', action: handler1, hotkey: 'a' }],
-        });
-
-        const user = userEvent.setup();
-        await user.keyboard('a');
-        expect(handler1).toHaveBeenCalledTimes(1);
     });
 });
