d5e7faaac1da17f52d336dbf01f6c411b88f8eeb
Ava Pun <37046477+AvaLovelace1@users.noreply.github.com>
2025-06-27 23:26:11 -0700

    refactor timer tests

--- src/lib/components/Timer.svelte.test.ts
+++ src/lib/components/Timer.svelte.test.ts
@@ -3,27 +3,22 @@ import { render, screen } from "@testing-library/svelte";
 import Timer from "$lib/components/Timer.svelte";
 
 describe("Timer.svelte", () => {
-    test("timeRemaining 0", async () => {
-        render(Timer, { time: 0 });
+    test.each([
+        [0, "0:00", "0ms"],
+        [3600, "1:00:00", "1h"],
+        [-3610, "-1:00:10", "-1h 10s"],
+    ])("timeRemaining %i", (timeRemaining, clocktime, datetime) => {
+        render(Timer, { time: timeRemaining });
         const timeElement = screen.getByRole("time");
-        expect(timeElement).toHaveTextContent(/^0:00$/);
-        expect(timeElement).toHaveAttribute("datetime", "0ms");
+        expect(timeElement).toHaveTextContent(clocktime);
+        expect(timeElement).toHaveAttribute("datetime", datetime);
     });
-    test("timeRemaining 3600", async () => {
-        render(Timer, { time: 3600 });
-        const timeElement = screen.getByRole("time");
-        expect(timeElement).toHaveTextContent(/^1:00:00$/);
-        expect(timeElement).toHaveAttribute("datetime", "1h");
-    });
-    test("timeRemaining -3610", async () => {
-        render(Timer, { time: -3610 });
-        const timeElement = screen.getByRole("time");
-        expect(timeElement).toHaveAttribute("datetime", "-1h 10s");
-    });
+
     test("custom class is applied", async () => {
         render(Timer, { time: 10, class: "custom-class" });
         expect(screen.getByRole("timer")).toHaveClass("custom-class");
     });
+
     test("radial progress is correct", async () => {
         render(Timer, { time: 30, maxTime: 120 });
         expect(screen.getByRole("progressbar")).toHaveValue(75);
