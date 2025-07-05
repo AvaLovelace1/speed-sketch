9aaa0e0d2c2140cdbd98bc51280bcd688fd74fba
Ava Pun <37046477+AvaLovelace1@users.noreply.github.com>
2025-07-05 12:28:44 -0700

    Add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import { page } from "$app/state";
+    import { goto } from "$app/navigation";
+    import { Button } from "bits-ui";
+    import Background from "$lib/components/Background.svelte";
+</script>
+
+<svelte:head>
+    <title>SpeedSketch - error</title>
+</svelte:head>
+
+<div class="flex h-dvh items-center-safe justify-center-safe">
+    <Background />
+    <div class="text-center">
+        <h1 class="mb-4 text-3xl font-semibold">Error {page.status}: {page.error?.message}</h1>
+        <Button.Root class="btn" onclick={async () => await goto("/", { replaceState: true })}>
+            Return to Main Menu
+        </Button.Root>
+    </div>
+</div>
