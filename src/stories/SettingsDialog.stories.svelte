5df2b76fd3fb3eb18761ed3bfed065ad00cc6103
Ava Pun <37046477+AvaLovelace1@users.noreply.github.com>
2025-07-14 20:27:21 -0700

    Inline dialogs into Storybook Stories

--- src/stories/SettingsDialog.stories.svelte
+++ src/stories/SettingsDialog.stories.svelte
@@ -1,13 +1,20 @@
 <script module lang="ts">
     import { defineMeta } from "@storybook/addon-svelte-csf";
-    import SettingsDialog from "./SettingsDialog.svelte";
+    import SettingsDialog from "$lib/components/dialog/SettingsDialog.svelte";
 
     const { Story } = defineMeta({
         title: "SettingsDialog",
         component: SettingsDialog,
         tags: ["autodocs"],
     });
+
+    let settingsDialog: SettingsDialog;
 </script>
 
 <!-- User settings dialog. -->
-<Story name="SettingsDialog" />
+<Story name="SettingsDialog">
+    {#snippet template(args)}
+        <button class="btn" onclick={() => settingsDialog.open()}>Settings</button>
+        <SettingsDialog bind:this={settingsDialog} {...args} />
+    {/snippet}
+</Story>
