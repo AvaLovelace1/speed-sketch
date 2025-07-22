1c5127d77b4057e463236a000a72c083780ab4a5
Ava Pun <37046477+AvaLovelace1@users.noreply.github.com>
2025-07-21 23:23:30 -0700

    Add Timer stories

---
+++ src/lib/components/Timer.stories.svelte
@@ -0,0 +1,16 @@
+<script module lang="ts">
+    import { defineMeta } from "@storybook/addon-svelte-csf";
+    import Timer from "./Timer.svelte";
+
+    const { Story } = defineMeta({
+        title: "Components/Timer",
+        component: Timer,
+        tags: ["autodocs"],
+    });
+</script>
+
+<!-- Displays a given time in a StatusAlert with a radial progress bar. -->
+<Story name="Default" args={{ time: 30, maxTime: 30 }} />
+
+<!-- Timer turns red when time is ≤ criticalTime. -->
+<Story name="Critical Time" args={{ time: 5, criticalTime: 10, maxTime: 30 }} />
