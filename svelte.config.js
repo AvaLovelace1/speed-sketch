889660589b55e3e8d703815f6e8c8bb3c5c7c915
Ava Pun <37046477+AvaLovelace1@users.noreply.github.com>
2025-07-27 18:36:42 -0700

    Add fallback 404 page

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,9 @@ import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 const config = {
     preprocess: vitePreprocess(),
     kit: {
-        adapter: adapter(),
+        adapter: adapter({
+            fallback: "404.html",
+        }),
         paths: {
             base: process.argv.includes("dev") ? "" : process.env.BASE_PATH,
         },
