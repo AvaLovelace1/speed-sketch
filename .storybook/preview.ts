2dbe2db4b35d5f47f022b2765cdcc1ad7502a9ac
Ava Pun <37046477+AvaLovelace1@users.noreply.github.com>
2025-08-22 12:23:37 -0400

    Remove color picker from storybook settings

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -13,7 +13,6 @@ const preview: Preview = {
 
         controls: {
             matchers: {
-                color: /(background|color)$/i,
                 date: /Date$/i,
             },
         },
