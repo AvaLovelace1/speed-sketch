80cac976e546a4bf65be12e5ae401a525d3c8686
Ava Pun <37046477+AvaLovelace1@users.noreply.github.com>
2026-04-09 14:05:07 -0400

    Add ListManager class

---
+++ src/lib/components/input/list-manager.svelte.ts
@@ -0,0 +1,58 @@
+/**
+ * Generic state manager for a reorderable list with single-item selection.
+ */
+export class ListManager<T> {
+    #selectedIdx: number;
+
+    constructor(public readonly items: T[]) {
+        this.#selectedIdx = $state(items.length > 0 ? 0 : -1);
+    }
+
+    // If -1, means that nothing is selected.
+    get selectedIdx() {
+        return this.#selectedIdx;
+    }
+
+    set selectedIdx(value: number) {
+        if (value < -1 || value >= this.items.length) {
+            throw new Error(`selectedIdx out of range: ${value}`);
+        }
+        this.#selectedIdx = value;
+    }
+
+    addItem = (newItem: T) => {
+        this.items.splice(this.selectedIdx + 1, 0, newItem);
+        this.selectedIdx++;
+    };
+
+    removeItem = () => {
+        this.items.splice(this.selectedIdx, 1);
+        this.selectedIdx = Math.min(this.selectedIdx, this.items.length - 1);
+    };
+
+    moveItemUp = () => {
+        if (this.selectedIdx === 0) return;
+        const item = this.items[this.selectedIdx];
+        this.items[this.selectedIdx] = this.items[this.selectedIdx - 1];
+        this.items[this.selectedIdx - 1] = item;
+        this.selectedIdx--;
+    };
+
+    moveItemDown = () => {
+        if (this.selectedIdx === this.items.length - 1) return;
+        const item = this.items[this.selectedIdx];
+        this.items[this.selectedIdx] = this.items[this.selectedIdx + 1];
+        this.items[this.selectedIdx + 1] = item;
+        this.selectedIdx++;
+    };
+
+    moveItem = (newIdx: number) => {
+        if (newIdx < 0 || newIdx >= this.items.length) {
+            throw new Error(`newIdx out of range: ${newIdx}`);
+        }
+        const item = this.items[this.selectedIdx];
+        this.removeItem();
+        this.selectedIdx = newIdx - 1;
+        this.addItem(item);
+    };
+}
