dde7ebb0ddd9a5029f22a4530fee842f6d8139e8
Ava Pun <37046477+AvaLovelace1@users.noreply.github.com>
2026-08-17 13:30:00 -0600

    refactor: Create CornerButton component

---
+++ src/lib/components/CornerButton.svelte
@@ -0,0 +1,25 @@
+<!--
+@component
+Button that appears fixed in the top right corner.
+-->
+<script lang="ts">
+    import Tooltip from "$lib/components/Tooltip.svelte";
+    import type { SvelteHTMLElements } from "svelte/elements";
+
+    type Props = {
+        name: string;
+        icon: string;
+    } & SvelteHTMLElements["button"];
+
+    const { name, icon, ...props }: Props = $props();
+</script>
+
+<Tooltip
+    side="left"
+    onclick={props.onclick}
+    class={["btn fixed right-4 btn-circle size-12 rounded-full text-xl shadow-sm", props.class]}
+>
+    <span class="iconify {icon}"></span>
+    <span class="sr-only">{name}</span>
+    {#snippet tooltipContent()}<p>{name}</p>{/snippet}
+</Tooltip>
--- src/lib/components/SettingsButton.svelte
+++ src/lib/components/SettingsButton.svelte
@@ -1,14 +1,11 @@
 <script lang="ts">
     import { appSettingsDialog } from "$lib/store/app-settings.svelte";
-    import Tooltip from "$lib/components/Tooltip.svelte";
+    import CornerButton from "$lib/components/CornerButton.svelte";
 </script>
 
-<Tooltip
-    side="left"
+<CornerButton
+    name="Settings"
+    icon="lucide--settings"
     onclick={() => appSettingsDialog.component?.open()}
-    class="btn fixed top-4 right-4 btn-circle size-12 rounded-full text-xl shadow-sm"
->
-    <span class="iconify lucide--settings"></span>
-    <span class="sr-only">Settings</span>
-    {#snippet tooltipContent()}<p>Settings</p>{/snippet}
-</Tooltip>
+    class="top-4"
+/>
